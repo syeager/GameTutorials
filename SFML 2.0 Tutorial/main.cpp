d4bc3f36ed9c9c10c95be10446dd3d28def421aa
Steve Yeager <steve.f.yeager@gmail.com>
2013-04-19 14:34:15 -0400

    Audio
    Video 21

--- SFML 2.0 Tutorial/main.cpp
+++ SFML 2.0 Tutorial/main.cpp
@@ -1,34 +1,53 @@
-// Video: 17
+// Video: 22
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 #include <iostream>
 #include <string>
 
 int main()
 {
 	sf::Vector2i screenDimensions(800, 600);
-	sf::Vector2i blockDimensions(10, 10);
 
 	sf::RenderWindow Window(sf::VideoMode(screenDimensions.x, screenDimensions.y, 32), "SFML Sample Application");
 	sf::Clock clock;
 
-	Window.setKeyRepeatEnabled(false);
-	srand(time(0));
+	sf::Texture bTexture;
+	sf::Sprite bImage;
 
-	float frameCounter = 0, switchFrame = 100, frameSpeed = 500;
-	bool updateFrame = true;
+	if (!bTexture.loadFromFile("resources/images/Background.jpg")) std::cout<<"Can't load image.\n";
+	bImage.setTexture(bTexture);
+	bImage.setScale(1.0f, (float)screenDimensions.y/bTexture.getSize().y);
 
-	sf::Texture pTexture;
-	sf::Sprite player;
-	if (!pTexture.loadFromFile("resources/images/Player_SS.png")) std::cout<<"error\n";
-	player.setTexture(pTexture);
-	float speed = 2;
-	
+	sf::RectangleShape rect(sf::Vector2f(20, 20));
+	rect.setFillColor(sf::Color::Red);
+	sf::RectangleShape rect2(sf::Vector2f(20, 20));
+	rect2.setFillColor(sf::Color::Blue);
 
-	enum Directions {Down, Left, Right, Up};
-	sf::Vector2i source(1, Down);
+	float moveSpeed = 100000;
+
+	sf::View view1, view2;
+	view1.setViewport(sf::FloatRect(0, 0, 0.5f, 1.0f));
+	view1.setSize(screenDimensions.x/2, screenDimensions.y);
+	view2.setViewport(sf::FloatRect(0.5f, 0, 0.5f, 1.0f));
+	view2.setSize(screenDimensions.x/2, screenDimensions.y);
+
+	sf::Vector2f position(screenDimensions.x/2, screenDimensions.y/2);
+	sf::Vector2f position2(position);
+
+	sf::SoundBuffer soundBuffer;
+	sf::Sound sound;
+
+	sf::Music music;
+	if (!music.openFromFile("resources/audio/jump.wav")) std::cout<<"Can't load music.\n";
+	music.play();
+	music.setLoop(true);
+
+	if (!soundBuffer.loadFromFile("resources/audio/jump.wav")) std::cout<<"Can't load audio.\n";
+	sound.setBuffer(soundBuffer);
 
 	while (Window.isOpen())
 	{
+		clock.restart();
 		sf::Event Event;
 		while (Window.pollEvent(Event))
 		{
@@ -39,78 +58,72 @@ int main()
 				Window.close();
 				break;
 
+			// keyboard
+			case sf::Event::KeyPressed:
+				if (Event.key.code == sf::Keyboard::Escape)
+					Window.close();
+				else if (Event.key.code == sf::Keyboard::P)
+				{
+					sound.play();
+				}
+				break;
+
 			default:
 				break;
 			}
-		}
 
+			// input
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+				rect.move(moveSpeed * clock.getElapsedTime().asSeconds(), 0);
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+				rect.move(-moveSpeed * clock.getElapsedTime().asSeconds(), 0);
 
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+				rect2.move(moveSpeed * clock.getElapsedTime().asSeconds(), 0);
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+				rect2.move(-moveSpeed * clock.getElapsedTime().asSeconds(), 0);
 
-		Window.clear();
-
-		for (int i = 0; i < screenDimensions.x/blockDimensions.x; i++)
-		{
-			for (int j = 0; j < screenDimensions.y/blockDimensions.y; j++)
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Z))
 			{
-				sf::VertexArray vArray(sf::PrimitiveType::LinesStrip, 4);
-				vArray[0].position = sf::Vector2f(i*blockDimensions.x, j*blockDimensions.y);
-				vArray[1].position = sf::Vector2f(i*blockDimensions.x+ blockDimensions.x, j*blockDimensions.y);
-				vArray[2].position = sf::Vector2f(i*blockDimensions.x + blockDimensions.x, j*blockDimensions.y + blockDimensions.y);
-				vArray[3].position = sf::Vector2f(i*blockDimensions.x, j*blockDimensions.y+blockDimensions.y);
-
-				for(int k = 0; k < 4; k++)
-				{
-					int red = rand() % 255;
-					int blue = rand() % 255;
-					int green = rand() % 255;
-
-					vArray[k].color = sf::Color(red, green, blue);
-				}
-
-				Window.draw(vArray);
-			}
-		}
-
-			sf::Vector2f moveSpeed(sf::Joystick::getAxisPosition(1, sf::Joystick::X), sf::Joystick::getAxisPosition(1, sf::Joystick::Y));
-
-			if (moveSpeed.x > 10)
-			{
-				source.y = Right;
-			}
-			else if (moveSpeed.x < -10)
-			{
-				source.y = Left;
-			}
-			else if (moveSpeed.y < -10)
-			{
-				source.y = Up;
-			}
-			else if (moveSpeed.y > 10)
-			{
-				source.y = Down;
-			}
-			player.move(moveSpeed.x * clock.getElapsedTime().asSeconds(), moveSpeed.y * clock.getElapsedTime().asSeconds());
-
-
-
-			// frame management		
-			frameCounter = (updateFrame) ? frameCounter + frameSpeed * clock.restart().asSeconds() : 0;
-
-			if (frameCounter >= switchFrame) 
-			{
-				frameCounter = 0;
-			
-				source.x++;
-				if (source.x*32 >= pTexture.getSize().x) source.x = 1;
+				view1.zoom(1.001f);
+				view2.zoom(0.999f);
 			}
 
-			
-			
-			player.setTextureRect(sf::IntRect(source.x*32, source.y*32, 32, 32));
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::R))
+			{
+				view1.rotate(0.1f);
+				view2.rotate(-0.1f);
+			}
 
-			Window.draw(player);
+
+			if (rect.getPosition().x+10 >= view1.getSize().x/2)
+				position.x = rect.getPosition().x+10;
+			else
+				position.x = view1.getSize().x/2;
+
+			if (rect2.getPosition().x+10 >= view2.getSize().x/2)
+				position2.x = rect2.getPosition().x+10;
+			else
+				position2.x = view2.getSize().x/2;
+
+			view1.setCenter(position);
+			view2.setCenter(position2);
+
+			// draw
+			Window.clear();
+
+			Window.setView(view1);
+			Window.draw(bImage);
+			Window.draw(rect);
+			Window.draw(rect2);
+
+			Window.setView(view2);
+			Window.draw(bImage);
+			Window.draw(rect2);
+			Window.draw(rect);
+
 			Window.display();
-		
+		}
 	}
 
 	return 0;
